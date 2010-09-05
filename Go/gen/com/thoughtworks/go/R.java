c99264213824f81036db5f293dc7a918d7c8c87c
Puneet Goyal <pungoyal@gmail.com>
2010-09-05 21:01:49 +0530

    adding the icon. looks like crap right now.

--- Go/gen/com/thoughtworks/go/R.java
+++ Go/gen/com/thoughtworks/go/R.java
@@ -16,7 +16,8 @@ public final class R {
         public static final int text=0x7f050001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int go=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int about_button=0x7f080001;
