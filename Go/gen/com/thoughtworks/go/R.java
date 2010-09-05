e40f932ece616bf4439160aec56a7bcab690feaf
Puneet Goyal <pungoyal@gmail.com>
2010-09-05 14:34:46 +0530

    coloring

--- Go/gen/com/thoughtworks/go/R.java
+++ Go/gen/com/thoughtworks/go/R.java
@@ -12,17 +12,8 @@ public final class R {
     }
     public static final class color {
         public static final int background=0x7f050000;
-        public static final int card_background=0x7f050004;
-        public static final int card_description=0x7f050006;
-        public static final int card_header_background=0x7f05000a;
-        public static final int card_number=0x7f050007;
-        public static final int card_properties_background=0x7f05000b;
-        public static final int card_property=0x7f050008;
-        public static final int card_property_value=0x7f050009;
-        public static final int card_title=0x7f050005;
-        public static final int dummy_text=0x7f050003;
-        public static final int murmurs_background=0x7f050001;
-        public static final int text=0x7f050002;
+        public static final int status_background=0x7f050002;
+        public static final int text=0x7f050001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
@@ -40,11 +31,12 @@ public final class R {
         public static final int cctray=0x7f040000;
     }
     public static final class string {
-        public static final int about_label=0x7f060002;
+        public static final int about_label=0x7f060003;
+        public static final int app_full_name=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int empty=0x7f060005;
-        public static final int exit_label=0x7f060003;
-        public static final int pipeline_status_title=0x7f060004;
-        public static final int view_status_label=0x7f060001;
+        public static final int empty=0x7f060006;
+        public static final int exit_label=0x7f060004;
+        public static final int pipeline_status_title=0x7f060005;
+        public static final int view_status_label=0x7f060002;
     }
 }
