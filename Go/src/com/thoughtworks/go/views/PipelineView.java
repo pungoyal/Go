03a533f94509cdd844e4fffecd4c4decbb228b2a
Puneet Goyal <pungoyal@gmail.com>
2010-09-05 14:56:11 +0530

    colors based on build status

--- Go/src/com/thoughtworks/go/views/PipelineView.java
+++ Go/src/com/thoughtworks/go/views/PipelineView.java
@@ -2,8 +2,8 @@ package com.thoughtworks.go.views;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.thoughtworks.go.domain.BuildActivity;
 import com.thoughtworks.go.domain.BuildStatus;
@@ -14,18 +14,16 @@ public class PipelineView extends LinearLayout {
 	public PipelineView(Context context, Pipeline pipeline) {
 		super(context);
 
-		TextView name = new TextView(context);
+		Button name = new Button(context);
 		name.setText(pipeline.getName());
-		name.setTextColor(getColor(pipeline.getLastBuildStatus(), pipeline.getActivity()));
+		name.setBackgroundColor(getColor(pipeline.getLastBuildStatus(), pipeline.getActivity()));
+		name.setTextColor(Color.BLACK);
 		addView(name, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
 	}
 
 	private int getColor(BuildStatus lastStatus, BuildActivity activity) {
-
-		if (activity == BuildActivity.BUILDING) {
+		if (activity == BuildActivity.BUILDING)
 			return lastStatus == BuildStatus.SUCCESS ? Color.YELLOW : Color.rgb(255, 128, 64);
-		}
-
 		return lastStatus == BuildStatus.SUCCESS ? Color.GREEN : Color.RED;
 	}
 }
